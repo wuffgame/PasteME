1be6108bf07c55d277e551114b40626d2f1e26fd
wuffgame <kacper5555544444@gmail.com>
2026-07-12 12:06:14 +0200

    Added a prototype that generates a file containing a custom UUID and text submitted by a user.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import fastapi
+from fastapi import FastAPI, Request
 import shortuuid
 import redis
 import uvicorn
 
 
+app = FastAPI()
+
+@app.post("/")
+async def root(request: Request):
+    text = await request.body()
+    text_after = text.decode("utf-8", errors="ignore")
+    uuid = shortuuid.uuid()
+    short_uuid = uuid[:4]
+    with open(f"paste/paste_{short_uuid}", "w") as f:
+        f.write(text_after)
+    return text_after
