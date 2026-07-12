eb9903478594422a5856ba508965c278f6826a8f
wuffgame <kacper5555544444@gmail.com>
2026-07-12 12:16:36 +0200

    A prototype has been added that displays a plain text file (or so I think) after you enter the received link.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 import shortuuid
 import redis
 import uvicorn
@@ -11,7 +12,15 @@ async def root(request: Request):
     text = await request.body()
     text_after = text.decode("utf-8", errors="ignore")
     uuid = shortuuid.uuid()
-    short_uuid = uuid[:4]
+    short_uuid = uuid[:8]
     with open(f"paste/paste_{short_uuid}", "w") as f:
         f.write(text_after)
-    return text_after
+    return f"http://127.0.0.1:8000/{short_uuid}"
+
+@app.get("/{id}", response_class=PlainTextResponse)
+async def get_paste(id: str):
+    try:
+        with open(f"paste/paste_{id}", "r") as f:
+            return f.read()
+    except Exception as e:
+        return f"Error: {e}"
